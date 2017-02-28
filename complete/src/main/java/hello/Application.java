821add62aea145e58519faae9b3c57498d50f81d
amriteya <amriteyapandey@gmail.com>
2017-02-28 22:40:25 +0530

    Added run capabilities in application

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,43 @@
 package hello;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Autowired
+	private MovieRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		// save a couple of movies
+		repository.save(new Movie("tt1979320", "Rush", 2013, Arrays.asList("Action", "Biography", "Drama")));
+		repository.save(new Movie("tt0111164", "The Shawshank Redemption", 1994, Arrays.asList("Crime", "Drama")));
+
+		List<Movie> response = repository.findByName("Rush");
+		System.out.println(response);
+
+		response = repository.findByGenre("Drama");
+		System.out.println(response);
+
+		response = repository.findByGenreIn(Arrays.asList("Drama"));
+		System.out.println(response);
+
+		response = repository.findByYear(2013);
+		System.out.println(response);
+
+		response = repository.findByYearBefore(2000);
+		System.out.println(response);
+	}
 }
